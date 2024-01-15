c59aaab1a030cb9466b2e16105174a47a8b229cd
anishshobithps <anish.shobith19@gmail.com>
2024-01-15 22:28:36 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { serve } from "@hono/node-server";
+import { bearerAuth } from "hono/bearer-auth";
+import { cors } from "hono/cors";
+import { prettyJSON } from "hono/pretty-json";
+import { Hono } from "hono";
+import { createTransport, SendMailOptions } from "nodemailer";
+import "dotenv/config";
+
+const app = new Hono();
+
+app.use("*", prettyJSON());
+app.use("*", cors());
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
+
+app.use("/*", bearerAuth({ token: process.env.TOKEN! }));
+
+const transporter = createTransport({
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+app.get("/", (c) => {
+  console.log(
+    process.env.EMAIL_ADDRESS,
+    process.env.EMAIL_PASSWORD,
+    process.env.TOKEN
+  );
+  return c.json({ message: "Welcome to my API" });
+});
+
+app.post("/sendemail", async (c) => {
+  const { name, email, subject, message } = await c.req.json();
+  const mailOptions: SendMailOptions = {
+    from: {
+      name,
+      address: process.env.EMAIL_ADDRESS!,
+    },
+    to: process.env.EMAIL_ADDRESS,
+    subject,
+    text: `
+    Email: ${email}
+    ${message}`,
+    html: `<b>${message}</b>`,
+  };
+
+  // Send email using the primary SMTP server
+  try {
+    await transporter.sendMail(mailOptions);
+    return c.json({ success: true, message: "Email sent successfully" }, 200);
+  } catch (error) {
+    console.log(error);
+    return c.json(
+      {
+        success: false,
+        message: "Error sending Email",
+      },
+      500
+    );
+  }
+});
+
+const port = parseInt(process.env.PORT!) || 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
